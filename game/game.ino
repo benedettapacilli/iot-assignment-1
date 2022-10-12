eadca0e2c90f529ba187a9f03031e2f9555ac850
expl0r3rgu1 <salvaddimando@gmail.com>
2022-10-12 11:51:19 +0200

    Started game phase

--- game/game.ino
+++ game/game.ino
@@ -5,14 +5,19 @@
 #define FADE_DELAY 30
 #define INITIAL_DELAY 10000000
 const int Ls = 3;
-const int L[] = {2, 4, 7, 8};
-const uint8_t B[] = {5, 6, 12, 13};
+const int L[] = {2, 4, 5, 6};
+const int B[] = {7, 8, 9, 10};
+int T[] = {0, 6000, 6000};
 bool initPhase = true;
 bool initMessagePrinted = false;
 int brightness = 0;
 int fadeStep = 5;
 bool sleeping = false;
 bool timerInitialized = false;
+int startTime = millis();
+bool beingPressed = false;
+int score = 0;
+int penalties = 0;
 
 void fadeRed()
 {
@@ -30,14 +35,13 @@ void fadeRed()
 
 void initInitialState()
 {
-    Serial.println("WAKING UP");
-    //sleep_disable();
     sleeping = false;
     initPhase = true;
     brightness = 0;
     initMessagePrinted = false;
     initPhase = true;
     timerInitialized = false;
+    startTime = millis();
 }
 
 bool isStartButtonPressed()
@@ -45,18 +49,90 @@ bool isStartButtonPressed()
     return digitalRead(B[0]) == HIGH;
 }
 
+bool isAButtonPressed()
+{
+    return digitalRead(B[0]) == HIGH || digitalRead(B[1]) == HIGH || digitalRead(B[2]) == HIGH || digitalRead(B[3]) == HIGH;
+}
+
+void givePenalty()
+{
+    penalties++;
+    Serial.println("Penalty!");
+    digitalWrite(Ls, HIGH);
+    delay(1000);
+    digitalWrite(Ls, LOW);
+    if (penalties > 3)
+    {
+        // END GAME
+    }
+}
+
 void deepSleep()
 {
     sleeping = true;
     digitalWrite(Ls, LOW);
     delay(100);
-    //Serial.flush();
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
     sleep_mode();
+    Serial.println("WAKING UP");
     sleep_disable();
-    delay(250);
-    Serial.println("AAAAAA");
+    delay(1000);
+}
+
+void game()
+{
+    digitalWrite(Ls, LOW);
+    Serial.println("Go!");
+    delay(T[0]);
+    int pattern[4];
+
+    Serial.println("Turning leds on");
+    for (int i = 0; i < 4; i++)
+    {
+        pattern[i] = random(0, 2);
+        digitalWrite(L[i], pattern[i]);
+    }
+    int patternOnTime = millis();
+    while (true)
+    {
+        if (millis() - patternOnTime > T[1])
+            break;
+
+        if (isAButtonPressed())
+            givePenalty();
+    }
+    Serial.println("Turning leds off");
+    for (int i = 0; i < 4; i++)
+    {
+        digitalWrite(L[i], LOW);
+    }
+
+    int startGameTime = millis();
+    while (true)
+    {
+        if (millis() - startGameTime < T[2])
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (digitalRead(B[i]) == HIGH)
+                {
+                    if (pattern[i] == 0)
+                    {
+                        givePenalty();
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+        }
+        else
+        {
+            givePenalty();
+            break;
+        }
+    }
 }
 
 void setup()
@@ -69,17 +145,17 @@ void setup()
         pinMode(B[i], INPUT);
         enableInterrupt(B[i], initInitialState, RISING);
     }
-    
+    randomSeed(analogRead(0));
+    T[0] = random(3000, 7000);
 }
 
 void loop()
 {
     if (initPhase)
     {
-        if(!timerInitialized) {
-            timerInitialized = true;
-            Timer1.initialize(INITIAL_DELAY);
-            Timer1.attachInterrupt(deepSleep);
+        if (millis() - startTime > 10000)
+        {
+            deepSleep();
         }
         fadeRed();
         if (!initMessagePrinted)
@@ -88,12 +164,16 @@ void loop()
             initMessagePrinted = true;
         }
     }
-    
-    if (isStartButtonPressed())
+
+    bool buttonPressed = isStartButtonPressed();
+    if (buttonPressed && !beingPressed)
     {
+        beingPressed = true;
         initPhase = false;
-        // TODO: Complete game
-        
+        game();
     }
-    
+    else if (!buttonPressed)
+    {
+        beingPressed = false;
+    }
 }
